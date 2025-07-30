279675e9b24cc856f6089ddbe72dcea805f4e4df
Sathyanarayanansakthi <sathyanarayanansakthi@gmail.com>
2025-07-30 22:20:03 +0530

    LeetCode added

---
+++ Loops/Forloop/forLoop p2.py
@@ -0,0 +1,35 @@
+# Print multiplication table of 5
+
+for i in range(0,11):
+    print(f"5 x {i} = {i * 5}")
+
+# Print each character in the string "Python"
+for char in "Python":
+    print(char)
+
+# Find the sum of numbers from 1 to 11
+total = 0
+for j in range(1,11):
+    total += j
+print("Sum:", total)
+
+#Print all Even Numbers form 1 to 50
+for s in range(0,51,2):
+    print(s)
+#Using If Statement
+for s in range(1,51):
+    if s % 2 == 0:
+        print(s)
+
+#Print all Odd numbers form 1 to 50
+for k in range(1,51,2):
+    print(k)
+
+#Using if Statement
+for k in range(0,50,1):
+    if k % 2 != 0:
+        print(k)
+
+# Print 7 Table
+for ji in range(1,11):
+    print(f"7X {ji} = {ji*7}")
---
+++ datatypes/strings/formatedStrings.py
@@ -0,0 +1,22 @@
+"""
+What is Formated Stirngs?
+Formatted string is a special type of string that lets you insert variables, values, or expressions directly into the string
+making it dynamic and more readable.
+
+
+Where we need to use Formated Strings?
+Formatted strings in Python are used whenever you need to insert values, variables, or expressions into a string
+"""
+
+#This is Old Version Code
+name = "Sathya"
+age = 21
+
+print("My name is {} and I am {} years old.".format(name, age))
+
+# THis is NEw Version code
+
+name = "Sathya"
+score = 85
+print(f"{name} scored {score} marks.")
+
