62417507665ddabd7b31240bcfef94826d82fe7f
Sathyanarayanansakthi <sathyanarayanansakthi@gmail.com>
2025-07-31 15:00:50 +0530

    EX

---
+++ operators/Ex/p1.py
@@ -0,0 +1,13 @@
+# Q1.Add two numbers
+a=10; b=20 ; print(f"The Answer is {a + b}")
+
+# Q2. Find the remainder when a is divided by b.
+
+a= 10; b=20 ; print(f"The Answer is {a % b}")
+
+# Calculate the square of a number using **.
+a= 10; b=20 ; print(f"The Answer is {a ** b}")
+
+# Q4.Convert minutes into hours and minutes
+
+# Q5. Swap two numbers using a temporary variable.
---
+++ operators/Ex/p2.py
@@ -0,0 +1,15 @@
+# 🔹 Q6. Check if a number is positive, negative, or zero.
+# 🔹 Q7. Check if a number is even or odd using %.
+# 🔹 Q8. Compare two numbers and print the greatest one.
+# 🔹 Q9. Use logical operators to check if:
+
+ # Q10. Use +=, -=, *= etc. to update a variable.
+""" Q11. Write a program that takes a number and:
+Adds 5
+
+Multiplies by 2
+
+Subtracts 10
+
+Divides by 2
+Using chained assignment operators."""
