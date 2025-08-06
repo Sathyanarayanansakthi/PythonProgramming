d6c271a2f56d035948c5e5bec9fc0ed11b038500
Sathyanarayanansakthi <sathyanarayanansakthi@gmail.com>
2025-08-06 14:47:04 +0530

    .

---
+++ CodeWars/Fundamentals/List Filtering.py
@@ -0,0 +1,19 @@
+#List Filtering
+
+"""In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]"""
+
+
+def filter_list(l):
+    # Remove all the str in the list a
+    # create a for loop  in and  check the item in the loop
+    # if the int need to print
+    result = []
+    for i in l:
+        if type(i) == int:
+            result.append(i)
+    return result
